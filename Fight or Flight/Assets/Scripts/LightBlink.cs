4d4eba2b59f46348a1305b41ce3a05d0c1ecbd07
Alex <stefan-alexandru.zarnescu@mail.bcu.ac.uk>
2023-02-25 08:25:38 +0000

    SkyCity

---
+++ Fight or Flight/Assets/Scripts/LightBlink.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightBlink : MonoBehaviour
+{
+
+    Light light;
+
+    float timer = 2; 
+    // Start is called before the first frame update
+    void Start()
+    {
+        light = GetComponent<Light>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer -= Time.deltaTime;
+
+        if (timer <= 0.5)
+        {
+            light.intensity = 0;
+
+            if (timer <= 0 )
+
+                timer = Random.Range(2, 4);
+        }
+        else
+            light.intensity = 3.5f;
+
+        Debug.Log(timer);
+    }
+}
