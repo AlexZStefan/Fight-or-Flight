afa3d7299aa6974b7953d9cafed389fb98391784
AlexZStefan <72689002+AlexZStefan@users.noreply.github.com>
2023-02-28 19:52:24 +0000

    Char Selection Update (#4)
    Added events to change the images for the selected char in game
    Added in game UI update with players avatar

--- Fight or Flight/Assets/Scripts/Player/Player.cs
+++ Fight or Flight/Assets/Scripts/Player/Player.cs
@@ -5,7 +5,30 @@ using UnityEngine;
 public class Player : ScriptableObject
 {
     public GameObject character;
-    public string characterSelected;
+    public string characterSelected = "";
 
     public Transform startingPosition;
+
+    public float stamina = 100;
+
+    int lowPunch  = 5;
+    int highPunch = 10;
+
+    public void CleanPlayer()
+    {
+        Destroy(character);
+        character = null;
+
+        characterSelected = "";
+    }
+
+    public void LowPunch(Player other)
+    {
+        other.stamina -= lowPunch;
+    }
+
+    public void HighPunch(Player other)
+    {
+        other.stamina -= highPunch;
+    }
 }
