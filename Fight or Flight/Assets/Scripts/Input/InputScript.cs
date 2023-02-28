c93815f02302b263e5802e1d30cfef7fb687cb2a
Huy Ngoc Nguyen <73152649+19162082@users.noreply.github.com>
2023-02-28 11:33:35 +0000

    Update on Animation

--- Fight or Flight/Assets/Scripts/Input/InputScript.cs
+++ Fight or Flight/Assets/Scripts/Input/InputScript.cs
@@ -12,6 +12,14 @@ public class InputScript : MonoBehaviour
     private float ranged;     // E
     private float melee;      // Q
 
+    private Animator anim;
+
+    void Start()
+    {
+
+        anim = GetComponent<Animator>();
+    }
+
     void Awake()
     {
         playerActions = new PlayerAct();
@@ -29,24 +37,36 @@ public class InputScript : MonoBehaviour
 
     private void FixedUpdate()
     {
+        crouch = playerActions.Player.Crouch.ReadValue<float>();
         movement = playerActions.Player.Movement.ReadValue<Vector2>();
+        jump = playerActions.Player.Jump.ReadValue<float>();
         if (movement != Vector2.zero)
         {
             Debug.Log("Movement: " + movement);
-            transform.position -= new Vector3(movement.x, 0, movement.y)*3 * Time.deltaTime;
-            transform.forward = new Vector3(-movement.x, 0, -movement.y) * Time.deltaTime;
-        }
-        crouch = playerActions.Player.Crouch.ReadValue<float>();
-        if(crouch != 0f) Debug.Log("Crouch: " + crouch);
+            transform.position -= new Vector3(movement.x, 0, movement.y) * 3 * Time.deltaTime;
 
-        jump = playerActions.Player.Jump.ReadValue<float>();
-        if (jump != 0f)
+            //animation for movement
+            anim.Play("Run");
+            transform.forward = new Vector3(-movement.x, 0, -movement.y) * Time.deltaTime;
+
+        }
+        else if (crouch != 0f)
+        {
+            Debug.Log("Crouch: " + crouch);
+            anim.Play("Crouch");
+        }
+        else if (jump != 0f)
         {
             Debug.Log("Jump: " + jump);
-
-
+            Debug.Log("Jump Animation played " + jump);
             // if is Grounded
             AudioManager.instance.PlayOneShot(FModEvents.instance.jumpSound, Vector3.zero);
+
+        }
+        else
+        {
+            // idle animation
+            anim.Play("Idle");
         }
         ranged = playerActions.Player.RangedAttack.ReadValue<float>();
         if (ranged != 0f) Debug.Log("Ranged: " + ranged);
@@ -54,4 +74,7 @@ public class InputScript : MonoBehaviour
         melee = playerActions.Player.LightAttack.ReadValue<float>();
         if (melee != 0f) Debug.Log("Melee: " + melee);
     }
+
 }
+
+
