6e515bb190ed06566745ed48c78e3f1bfec23ca1
Huy Ngoc Nguyen <73152649+19162082@users.noreply.github.com>
2023-03-01 10:46:14 +0000

    Animation (#5)
    * Update on Animation
    
    * Update at Animation
    
    * Update
    
    ---------
    
    Co-authored-by: AlexZStefan <72689002+AlexZStefan@users.noreply.github.com>

--- Fight or Flight/Assets/Scripts/Input/InputScript.cs
+++ Fight or Flight/Assets/Scripts/Input/InputScript.cs
@@ -12,6 +12,13 @@ public class InputScript : MonoBehaviour
     private float ranged;     // E
     private float melee;      // Q
 
+    private Animator anim;
+
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
     void Awake()
     {
         playerActions = new PlayerAct();
@@ -29,29 +36,55 @@ public class InputScript : MonoBehaviour
 
     private void FixedUpdate()
     {
+        crouch = playerActions.Player.Crouch.ReadValue<float>();
         movement = playerActions.Player.Movement.ReadValue<Vector2>();
+        jump = playerActions.Player.Jump.ReadValue<float>();
+        melee = playerActions.Player.LightAttack.ReadValue<float>();
+        ranged = playerActions.Player.RangedAttack.ReadValue<float>();
+
+
         if (movement != Vector2.zero)
         {
             Debug.Log("Movement: " + movement);
-            transform.position -= new Vector3(movement.x, 0, movement.y)*3 * Time.deltaTime;
-            transform.forward = new Vector3(-movement.x, 0, -movement.y) * Time.deltaTime;
-        }
-        crouch = playerActions.Player.Crouch.ReadValue<float>();
-        if(crouch != 0f) Debug.Log("Crouch: " + crouch);
+            transform.position -= new Vector3(movement.x, 0, movement.y) * 3 * Time.deltaTime;
 
-        jump = playerActions.Player.Jump.ReadValue<float>();
-        if (jump != 0f)
+            //animation for movement
+            anim.Play("Run");
+            transform.forward = new Vector3(-movement.x, 0, -movement.y) * Time.deltaTime;
+
+        }
+        else if (crouch != 0f)
+        {
+            Debug.Log("Crouch: " + crouch);
+            anim.Play("Crouch");
+        }
+        else if (jump != 0f)
         {
             Debug.Log("Jump: " + jump);
-
-
+            anim.Play("Jump");
             // if is Grounded
             AudioManager.instance.PlayOneShot(FModEvents.instance.jumpSound, Vector3.zero);
-        }
-        ranged = playerActions.Player.RangedAttack.ReadValue<float>();
-        if (ranged != 0f) Debug.Log("Ranged: " + ranged);
 
-        melee = playerActions.Player.LightAttack.ReadValue<float>();
-        if (melee != 0f) Debug.Log("Melee: " + melee);
+        }
+        else if (melee != 0f)
+        {
+            Debug.Log("Melee: " + melee);
+            anim.Play("Meelee");
+        }
+
+        else if (ranged != 0f) 
+        {
+            Debug.Log("Ranged: " + ranged);
+            anim.Play("Range");
+        }
+
+        else
+        {
+            // idle animation
+            anim.Play("Idle");
+        }
     }
+
 }
+
+
